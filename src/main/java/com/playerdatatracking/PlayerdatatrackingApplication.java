a2bfe372305e94794782ffd4fd0dc655e1b92541
Larrivey <r.marques.2018@alumnos.urjc.es>
2024-07-23 16:56:28 +0200

    feat: init commit

---
+++ src/main/java/com/playerdatatracking/PlayerdatatrackingApplication.java
@@ -0,0 +1,13 @@
+package com.playerdatatracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerdatatrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerdatatrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playerdatatracking/PlayerdatatrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playerdatatracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerdatatrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
